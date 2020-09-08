858c6f1d1fe9aa29aef525959673289cd169a840
Edison Girado A <1470@Holbertonschool.com>
2020-09-08 14:10:41 -0500

    file storage, original file.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,14 +68,3 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
-
-    def count(self, cls=None):
-        """counter"""
-        query = self.all(cls)
-        return len(query)
-
-    def get(self, cls, id):
-        """get an item"""
-        query = self.all(cls)
-        return query.get(cls + '.' + id)
-
