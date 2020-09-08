6051b458de6070d99dedafccfc3a9614bc9cb76c
Daniel Perez <1498@holbertonschool.com>
2020-09-08 17:16:41 +0000

    states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -24,3 +24,41 @@ def post_state():
     state.save()
     return (jsonify(state.to_dict()), 201)
 
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """get state"""
+    query = storage.all(State)
+    if "State.{}".format(state_id) not in query:
+        abort(404)
+    state_ = storage.get("State", state_id)
+    if state_ is None:
+        abort(404)
+    return jsonify(state_.to_dict())
+
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    """delete state"""
+    query = storage.all(State)
+    if "State.{}".format(state_id) not in query:
+        abort(404)
+    storage.delete(query["State.{}".format(state_id)])
+    storage.save()
+    return jsonify({})
+
+@app_views.route('/states/<state_id>', methods=['PUT'],strict_slashes=False)
+def put_state(state_id):
+    """update state"""
+    query = storage.all(State)
+    if "State.{}".format(state_id) not in query:
+        abort(404)
+    state = storage.get('State', state_id)
+    if state is None:
+        abort(404)
+    if not request.json:
+        abort(400, 'Not a JSON')
+    for i in request.json:
+        if i not in ['id', 'created_at', 'updated_at']:
+            setattr(state, i, request.json[i])
+    state.save()
+    return jsonify(state.to_dict())
+
